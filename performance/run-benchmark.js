d1220d9ec01a466c4d6e67e573d54d70b51166de
Marc Altmann <marcaltmann@posteo.de>
2017-10-28 16:10:16 +0200

    Remove old benchmark file

--- performance/run-benchmark.js
+++
@@ -1,9 +0,0 @@
-const runBenchmark = (name, test) => {
-  const start = new Date().getTime();
-  test();
-  const total = new Date().getTime() - start;
-
-  console.log(name + ': ' + total + 'ms');
-};
-
-export default runBenchmark;
