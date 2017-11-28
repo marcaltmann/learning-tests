a5f0fc9d5e3e52a01767fb2326848f33baeac2ab
Marc Altmann <marcaltmann@posteo.de>
2017-11-28 23:22:46 +0100

    window and global stuff

--- test/dom.js
+++ test/dom.js
@@ -6,5 +6,15 @@ describe('dom', function() {
     it('has window', function() {
       expect(document).not.to.be.undefined;
     });
+
+    it('treats window not like global', function() {
+      global.niceNewVariable = 'hello';
+      expect(window.niceNewVariable).not.to.equal('hello');
+    });
+
+    it('treats global like window', function() {
+      window.niceNewVariable = 'hello';
+      expect(global.niceNewVariable).to.equal('hello');
+    });
   });
 });
