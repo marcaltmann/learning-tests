c0b94fd04f5720f9d1ea3abe29e4eb0dfbd2bbe4
Marc Altmann <marcaltmann@posteo.de>
2017-12-02 16:14:52 +0100

    More basic d3 selection tests

--- test/d3js/chapter6.js
+++ test/d3js/chapter6.js
@@ -1,5 +1,6 @@
 const jsdom = require('jsdom-global');
 const d3 = require('d3');
+const sinon = require('sinon');
 const expect = require('chai').expect;
 
 describe('d3 basics', function() {
@@ -9,4 +10,48 @@ describe('d3 basics', function() {
     jsdom();
   });
 
+  describe('append(\'svg\')', function() {
+    it('creates an SVG', function() {
+      d3.select('body').append('svg');
+      expect(document.getElementsByTagName('svg')).to.have.length(1);
+    });
+
+    it('returns a selection', function() {
+      const svg = d3.select('body').append('svg');
+      expect(svg.constructor.name).to.equal('Selection');
+    });
+  });
+
+  describe('selection', function() {
+    let selection;
+    beforeEach(function() {
+      const body = d3.select('body');
+      body.append('svg');
+      body.append('svg');
+      body.append('svg');
+      selection = d3.selectAll('svg');
+    });
+
+    it('returns the size/length', function() {
+      expect(selection.size()).to.equal(3);
+    });
+
+    it('returns if it is empty', function() {
+      expect(selection.empty()).to.be.false;
+    });
+
+    it('returns one node', function() {
+      expect(selection.node().constructor.name).to.equal('SVGSVGElement');
+    });
+
+    it('returns multiple nodes', function() {
+      expect(selection.nodes()).to.have.length(3);
+    });
+
+    it('can iterate over itself', function() {
+      const callback = sinon.spy();
+      selection.each(callback);
+      expect(callback.calledThrice).to.be.true;
+    });
+  });
 });
