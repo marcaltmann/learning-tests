86d6f5b37e59752d0ad8b4bccf5f18123340023e
Marc Altmann <marcaltmann@posteo.de>
2017-12-02 15:45:26 +0100

    Draw column chart with divs

---
+++ test/d3js/basics.js
@@ -0,0 +1,59 @@
+const jsdom = require('jsdom-global');
+const d3 = require('d3');
+const expect = require('chai').expect;
+
+describe('d3 basics', function() {
+  beforeEach(function() {
+    jsdom();
+  });
+
+  it('should work', function() {
+    d3.select('body')
+      .append('p')
+      .text('New paragraph!');
+
+    const p = document.getElementsByTagName('p');
+    expect(p.length).to.equal(1);
+    expect(p[0].textContent).to.equal('New paragraph!');
+  });
+
+  it('nodes without bound data have should no __data__ key', function() {
+    d3.select('body')
+      .append('p')
+      .text('New paragraph!');
+
+    const selection = d3.selectAll('p');
+    expect(selection._groups[0][0]).not.to.have.key('__data__');
+  });
+
+  it('should render data', function() {
+    var dataset = [5, 10, 15, 20, 25];
+
+    d3.select('body').selectAll('p')
+      .data(dataset)
+      .enter()
+      .append('p')
+      .text(d => d);
+
+    const pCollection = document.getElementsByTagName('p');
+    expect(pCollection.length).to.equal(5);
+    expect(Array.from(pCollection).map(p => p.textContent)).to.deep.equal(
+      ['5', '10', '15', '20', '25']
+    );
+  });
+
+  it('nodes with bound data should have a __data__ key', function() {
+    var dataset = [5, 10, 15, 20, 25];
+
+    d3.select('body').selectAll('p')
+      .data(dataset)
+      .enter()
+      .append('p')
+      .text('New paragraph!');
+
+    const selection = d3.selectAll('p');
+    const node = selection._groups[0][0];
+    expect(node).to.have.key('__data__');
+    expect(node.__data__).to.equal(5);
+  });
+});
---
+++ test/d3js/chapter6.js
@@ -0,0 +1,12 @@
+const jsdom = require('jsdom-global');
+const d3 = require('d3');
+const expect = require('chai').expect;
+
+describe('d3 basics', function() {
+  const dataset = [5, 10, 15, 20, 25];
+
+  beforeEach(function() {
+    jsdom();
+  });
+
+});
