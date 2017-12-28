2ee680eec9df42851310f52e20d58622c4ade72d
Marc Altmann <marcaltmann@posteo.de>
2017-12-28 20:20:19 +0100

    Update ava tests

--- test/array.js
+++ test/array.js
@@ -7,14 +7,14 @@ test('indexOf returns -1 when the value is not present', (t) => {
 });
 
 test('splice should alter original array', (t) => {
-  const original = [1, 2, 3, 4, 5];
-  const actual = original.splice(2, 2);
+  const actual = [1, 2, 3, 4, 5];
+  actual.splice(2, 2);
   const expected = [1, 2, 5];
-  t.is(actual, expected);
+  t.deepEqual(actual, expected);
 });
 
 test('splice should return spliced portion', (t) => {
   const actual = [1, 2, 3, 4, 5].splice(2, 2);
   const expected = [3, 4];
-  t.is(actual, expected);
+  t.deepEqual(actual, expected);
 });
