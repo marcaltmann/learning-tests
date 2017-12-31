8a4a73f1c205b2f2e605d1d45163386301420672
Marc Altmann <marcaltmann@posteo.de>
2017-12-31 03:54:04 +0100

    Almost understood band scale padding

---
+++ test/d3/band-scale.ava.js
@@ -0,0 +1,46 @@
+import { scaleBand } from 'd3-scale';
+import test from 'ava';
+
+function basicScale() {
+  return scaleBand()
+    .domain([0, 1, 2, 3, 4])
+    .range([0, 100]);
+}
+
+test('mapping', (t) => {
+  const scale = basicScale();
+  const actual = scale(2);
+  const expected = 40;
+  t.is(actual, expected, 'domain value maps to start of the band within output range');
+});
+
+test('bandwidth', (t) => {
+  const scale = basicScale();
+  const actual = scale.bandwidth();
+  const expected = 20;
+  t.is(actual, expected);
+});
+
+test('step', (t) => {
+  const scale = basicScale();
+  const actual = scale.step();
+  const expected = 20;
+  t.is(actual, expected);
+});
+
+test('outer padding', (t) => {
+  // Read 12.5/15 as: two times 12.5 for the padding and 5 times 15 equals 100.
+  const scale = basicScale().paddingOuter(12.5/15);
+  const actual = [scale(0), scale(1), scale(2), scale(3), scale(4)];
+  const expected = [12.5, 27.5, 42.5, 57.5, 72.5];
+  t.deepEqual(actual, expected, 'paddingOuter is measured as a ratio of the bandwidth');
+});
+
+test('inner padding', (t) => {
+  // Read 5/6 as: 25 equally-sized parts:
+  // 1 band, 5 padding, 1 band, 5 padding, 1 band, 5 padding, 1 band, 5 padding, 1 band.
+  const scale = basicScale().paddingInner(5/6);
+  const actual = [scale(0), scale(1), scale(2), scale(3), scale(4)];
+  const expected = [0, 24, 48, 72, 96];
+  t.deepEqual(actual, expected, 'paddingInner is measured as a ratio of the bandwidth plus padding?');
+});
